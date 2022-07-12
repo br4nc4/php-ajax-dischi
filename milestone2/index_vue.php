97ad31a7260fde3470e74b8d2e85ccdc8bceae4d
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-07-12 17:59:38 +0200

    milestone2 start

---
+++ milestone2/index_vue.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+    <title>dischi-vue</title>
+</head>
+<body>
+    <div id="app">
+        <header>
+            <div class="d-flex">
+
+            </div>
+        </header>
+
+        <main>
+            <div class="container">
+
+            </div>
+        </main>
+    </div>
+    <script src="js/vue.js"></script>
+</body>
+</html>
